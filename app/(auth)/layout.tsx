07adbf9b26a894b1d6ecf3dc1ac325f0567ab124
sodusaiihan <m.sodoo9911@gmail.com>
2023-11-05 18:19:09 +0800

    remove auth folder

--- app/(auth)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const AuthLayout = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div className="w-full flex items-center justify-center">{children}</div>
-  );
-};
-
-export default AuthLayout;
